dcc808de18e81078929b066b48e59f489f42dcbc
knparamesh26 <knparameshwaran26@gmail.com>
2022-07-27 18:50:08 +0530

    Updated Home, Header files and Added bootstrap

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,15 @@
 import { h } from 'preact';
 import style from './style.css';
 import { Link } from 'preact-router';
-import pokemon from '../../assets/images/pokemon.jpeg'
+import pokemon from '../../assets/images/pokemon.png'
 
 
 const Home = () => (
-	<div class='container'>
-		<Link href='/pokemon' >
-			<img class='image-fluid' src={pokemon} alt='Pokemon'/>
+	<div class='container text-center' style='background-color: aqua'>
+		<Link class='text-decoration-none' href='/pokemon' >
+			<img class='w-50 p-5' src={pokemon} alt='Pokemon'/>
+
+			<h2>View all character</h2>
 		</Link>
 	</div>
 );
